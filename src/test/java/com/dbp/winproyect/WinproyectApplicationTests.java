f5d5ec759a7f9110ed8039b48299394805151829
Henry Quispe Sutta <Henryqs>
2024-09-30 15:08:59 -0500

    mi primercommit

---
+++ src/test/java/com/dbp/winproyect/WinproyectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dbp.winproyect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WinproyectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
