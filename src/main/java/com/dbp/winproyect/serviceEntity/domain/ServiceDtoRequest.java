93b55683b1c3b3be5fba429db754eb87db09f0d0
Henry Quispe Sutta <Henryqs>
2024-10-11 18:12:51 -0500

    posible borrar

--- src/main/java/com/dbp/winproyect/serviceEntity/domain/ServiceDtoRequest.java
+++ src/main/java/com/dbp/winproyect/serviceEntity/domain/ServiceDtoRequest.java
@@ -1,4 +1,12 @@
 package com.dbp.winproyect.serviceEntity.domain;
 
+import java.util.List;
+
 public class ServiceDtoRequest {
+
+    private String description;
+    private String nombre;
+    private Double suggestedPrice;
+
+    
 }
