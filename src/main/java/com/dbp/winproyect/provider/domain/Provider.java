ae143303c1309351de3e6d4f8dc910dcb68be1e4
Luis Millones <luismillonesutc9090@gmail.com>
2024-10-12 15:17:39 -0500

    cambios2

--- src/main/java/com/dbp/winproyect/provider/domain/Provider.java
+++ src/main/java/com/dbp/winproyect/provider/domain/Provider.java
@@ -8,10 +8,11 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
-@Data
+
 @Entity
-//@NoArgsConstructor
-//@RequiredArgsConstructor
+@NoArgsConstructor
+@RequiredArgsConstructor
+
 
 public class Provider extends AppUser {
     private Long ruc;
